cde90bd878d9ba9816c3fb868ab5ad7ddf4586ac
eminaliyevx <eminaliyevx@gmail.com>
2023-03-03 19:17:47 +0400

    ✨ Apply email confirmation to business module

--- apps/backend/src/business/business.controller.ts
+++ apps/backend/src/business/business.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+import { AuthService } from "src/auth/auth.service";
 import { Public } from "src/decorators/public.decorator";
 import { BusinessService } from "./business.service";
 import { CreateBusinessDto } from "./dto/create-business.dto";
@@ -7,12 +8,18 @@ import { CreateBusinessDto } from "./dto/create-business.dto";
 @ApiTags("Business")
 @Controller("business")
 export class BusinessController {
-  constructor(private readonly businessService: BusinessService) {}
+  constructor(
+    private readonly businessService: BusinessService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Public()
   @HttpCode(200)
   @Post("register")
   async register(@Body() createBusinessDto: CreateBusinessDto) {
-    return this.businessService.register(createBusinessDto);
+    const user = await this.businessService.register(createBusinessDto);
+    this.authService.sendConfirmationEmail(user);
+
+    return user;
   }
 }
--- apps/backend/src/business/business.module.ts
+++ apps/backend/src/business/business.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "src/auth/auth.module";
 import { PrismaService } from "src/prisma/prisma.service";
 import { BusinessController } from "./business.controller";
 import { BusinessService } from "./business.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [BusinessController],
   providers: [PrismaService, BusinessService],
 })
